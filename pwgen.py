0918b46955023df86b80dff129fc8e71241ec254
Sean Callaway <seancallaway@gmail.com>
2016-08-15 10:45:36 -0700

    Added docstrings and cleared some pylint errors.

--- pwgen.py
+++ pwgen.py
@@ -1,32 +1,40 @@
 #!/usr/bin/env python
+"""
+    pwgen.py
+    ~~~~~~~~
+
+    Generates a user-readible password from
+    a wordlist.
+
+    :copyright: (c) 2016 by Sean Callaway.
+    :license: GNU GPL v2, see LICENSE for more details.
+"""
 import sys
 import os
 import random
 
-total_bytes = 0
-file_name = "wordlist.txt"
-#file_name = "/usr/share/dict/words"
-spec_chars = ['@', '#', '$', '%', '*', '-', "_"]
+FILE_NAME = "wordlist.txt"
+#FILE_NAME = "/usr/share/dict/words"
+SPEC_CHARS = ['@', '#', '$', '%', '*', '-', "_"]
 MIN_WORD_LENGTH = 5
 MAX_WORD_LENGTH = 8
 
-def get_random_line():
-    random_point = random.randint(0, total_bytes)
-    file = open(file_name)
-    file.seek(random_point)
-    file.readline()
+def get_random_line(file_size):
+    """Returns a random line from the wordlist."""
+    random_point = random.randint(0, file_size)
+    myfile = open(FILE_NAME)
+    myfile.seek(random_point)
+    myfile.readline()
 
-    return file.readline()
-
-def init_file():
-    global total_bytes
-    total_bytes = os.stat(file_name).st_size
+    return myfile.readline()
 
 def get_spec_char():
-    return random.choice(spec_chars)
+    """Returns a random special character."""
+    return random.choice(SPEC_CHARS)
 
 def main():
-    init_file()
+    """Primary entry point for the program."""
+    total_bytes = os.stat(FILE_NAME).st_size
     num = 1
 
     if len(sys.argv) > 1:
@@ -36,11 +44,11 @@ def main():
     for i in range(0, num):
         word1 = ""
         while len(word1) < MIN_WORD_LENGTH or len(word1) > MAX_WORD_LENGTH:
-            temp = get_random_line()
+            temp = get_random_line(total_bytes)
             word1 = (''.join(j for j in temp if j.isalnum())).capitalize()
         word2 = ""
         while len(word2) < MIN_WORD_LENGTH or len(word2) > MAX_WORD_LENGTH:
-            temp = get_random_line()
+            temp = get_random_line(total_bytes)
             word2 = (''.join(j for j in temp if j.isalnum())).capitalize()
         num1 = random.randint(0, 9)
         num2 = random.randint(0, 9)
@@ -49,3 +57,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+

4ce2bbefbb8e33d031dade14c7b58c250bcf7cd5
Sean Callaway <seancallaway@gmail.com>
2017-06-26 10:50:43 -0700

    Reorganized as package.

---
+++ nupass/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+    nupass
+    ~~~~~~
+
+    Generates a user-readible password from
+    a wordlist.
+
+    :copyright: (c) 2017 by Sean Callaway.
+    :license: GNU GPL v2, see LICENSE for more details.
+"""
+
+from .nupass import gen_pass

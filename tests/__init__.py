5f4efdce240407575f1ca6208344dd11d14dea45
Sean Callaway <seancallaway@gmail.com>
2017-06-27 11:14:46 -0700

    Created unit tests.

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+    tests
+    ~~~~~
+
+    Unit tests for NuPass
+
+    :copyright: (c) 2017 by Sean Callaway.
+    :license: GNU GPL v2, see LICENSE for more details.
+"""

6b4e35d77be60716d704a95df151ace065a96560
Sean Callaway <seancallaway@gmail.com>
2017-06-29 10:38:00 -0700

    Added agruments to gen_pass(). Used instead of constants.
    Resolves #9.

--- nupass/nupass.py
+++ nupass/nupass.py
@@ -15,8 +15,6 @@ import random
 FILE_NAME = "wordlist.txt"
 #FILE_NAME = "/usr/share/dict/words"
 SPEC_CHARS = ['@', '#', '$', '%', '*', '-', "_"]
-MIN_WORD_LENGTH = 5
-MAX_WORD_LENGTH = 8
 
 FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 FULL_FILE_NAME = os.path.join(FILE_PATH, FILE_NAME)
@@ -28,6 +26,7 @@ def get_random_line(file_size):
     random_point = random.randint(0, file_size)
     myfile = open(FULL_FILE_NAME)
     myfile.seek(random_point)
+    myfile.readline()
     line = myfile.readline()
     myfile.close()
 
@@ -37,14 +36,14 @@ def get_spec_char():
     """Returns a random special character."""
     return random.choice(SPEC_CHARS)
 
-def gen_pass():
+def gen_pass(min_w_len=5, max_w_len=8):
     """Returns a readible temporary password."""
     word1 = ""
-    while len(word1) < MIN_WORD_LENGTH or len(word1) > MAX_WORD_LENGTH:
+    while len(word1) < min_w_len or len(word1) > max_w_len:
         temp = get_random_line(TOTAL_BYTES)
         word1 = (''.join(j for j in temp if j.isalnum())).capitalize()
     word2 = ""
-    while len(word2) < MIN_WORD_LENGTH or len(word2) > MAX_WORD_LENGTH:
+    while len(word2) < min_w_len or len(word2) > max_w_len:
         temp = get_random_line(TOTAL_BYTES)
         word2 = (''.join(j for j in temp if j.isalnum())).capitalize()
     num1 = random.randint(0, 9)

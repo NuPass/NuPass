9cc6954c6ac33a0a5b0d05f1be425cdfaab84f80
Sean Callaway <sean.callaway@actzero.ai>
2021-08-12 10:45:20 -0700

    Include README as long_description.

--- setup.py
+++ setup.py
@@ -9,9 +9,14 @@
 """
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(name='nupass',
       version='1.0.0',
       description='Generate readable, typable passwords',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Programming Language :: Python :: 2.7',
